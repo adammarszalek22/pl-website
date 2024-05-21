d2c4ce1e7995ba8883273d1329ac0a46d65e0efb
Adam Marszalek <adammarszalek60@gmail.com>
2024-05-21 07:46:33 +0100

    progress on the leagues page

--- controllers/page-controllers/leagues-controller.js
+++ controllers/page-controllers/leagues-controller.js
@@ -1,9 +1,9 @@
-const path = require('path');
+const fs = require('fs');
 
 const { myLeagues, leagues } = require('../../pl-server-api/leagues');
 
 module.exports.getMyLeaguesPage = async (req, res) => {
-
+    
     try {
 
         const myAdminLeagues = await myLeagues(req.session.accessToken);
@@ -16,15 +16,22 @@ module.exports.getMyLeaguesPage = async (req, res) => {
 
         const allLeagues = [...myAdminLeagues.leagues, ...joinedLeagues.leagues];
 
+        let leagueDivs = '';
         for (const league of allLeagues) {
-            addLeagueDiv(league);
+            leagueDivs += addLeagueDiv(league);
         }
 
-        // console.log(allLeagues)
+        // Getting raw HTML from the public folder
+        const rawHtml = await fs.promises.readFile(`${__dirname}/../../public/my-leagues.html`, 'utf-8');
 
-        res.sendFile(path.join(__dirname, '..', '..', 'public', 'my-leagues.html'));
+        // Amending the HTML
+        const completeHtml = rawHtml
+        .replace('%MY_LEAGUES%', leagueDivs);
+
+        res.send(completeHtml);
     
     } catch(error) {
+
         console.log(error)
         res
         .status(401)
@@ -40,12 +47,41 @@ const addLeagueDiv = (league) => {
 
     const positions = league.positions.split(' ').map(Number);
 
-    const orderedUsers = [];
+    let leagueDiv = `
+    <div class="league-small">
+        <div class="league-row league-headings">
+            <p>User</p>
+            <p>Position</p>
+            <p>Points</p>
+            <p>One pointers</p>
+            <p>Exact Scores</p>
+        </div>
+    `;
 
-    for (const position of positions) {
-        orderedUsers.push(league.user.find(user => user.id === position));
+    for (let i = 0; i < 3; i++) {
+
+        const userId = positions[i];
+
+        const user = league.user.find(user => user.id === userId);
+
+        leagueDiv += `
+        <div class="league-row league-leader">
+            <p>${user?.username ?? ''}</p>
+            <p>${user?.position ?? ''}</p>
+            <p>${user?.points ?? ''}</p>
+            <p>${user?.one_pointers ?? ''}</p>
+            <p>${user?.three_pointers ?? ''}</p>
+        </div>
+        `;
     }
 
-    
+    leagueDiv += `
+        <div class="show-league">
+            <button class="show-league-button">Show League</button>
+        </div>
+    </div>
+    `;
+
+    return leagueDiv;
 
 }
